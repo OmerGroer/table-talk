45c7dddb230322e7d18b0c31bc4bba858f4723dd
Omer Groer <drutr123@gmail.com>
2024-12-29 19:31:35 +0200

    main activity with navbar

---
+++ app/src/main/java/com/example/tabletalk/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.tabletalk
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+
+class MainActivity : AppCompatActivity() {
+    var navController: NavController? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val navHostFragment: NavHostFragment? =
+            supportFragmentManager.findFragmentById(R.id.main_nav_host) as? NavHostFragment
+        navController = navHostFragment?.navController
+        navController?.let {
+            NavigationUI.setupActionBarWithNavController(
+                activity = this,
+                navController = it
+            )
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> navController?.popBackStack()
+            else -> navController?.let { NavigationUI.onNavDestinationSelected(item, it) }
+        }
+        return true
+    }
+}
