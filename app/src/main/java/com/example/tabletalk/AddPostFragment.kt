2de17f2097673f9d3a8fdbae8d0c054f8f9be895
Omer Groer <drutr123@gmail.com>
2025-01-02 18:39:27 +0200

    add post

---
+++ app/src/main/java/com/example/tabletalk/AddPostFragment.kt
@@ -0,0 +1,20 @@
+package com.example.tabletalk
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AddPostFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_add_post, container, false)
+
+
+
+        return view
+    }
+}
