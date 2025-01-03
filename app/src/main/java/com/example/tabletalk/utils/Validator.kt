17902db0fc37c5eef6c45b6f560191fcc9219de2
galnaor86 <galnaor86@gmail.com>
2025-01-03 11:03:11 +0200

    log in page

---
+++ app/src/main/java/com/example/tabletalk/utils/Validator.kt
@@ -0,0 +1,18 @@
+package com.example.tabletalk.utils
+
+import android.util.Patterns
+
+class Validator {
+    private val _passwordPattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{6,}$".toRegex()
+    fun validateEmail(email: String?): Boolean {
+        return email != null && Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    fun validatePassword(password: String?): Boolean {
+        return password != null && _passwordPattern.matches(password)
+    }
+
+    fun validateConfirmPassword(password: String, confirmPassword: String): Boolean {
+        return password == confirmPassword
+    }
+}
